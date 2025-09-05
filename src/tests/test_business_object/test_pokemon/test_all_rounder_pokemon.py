ab66915ce82af5dbd70513210af30182c4c0c819
dieudo <dieudonne.toe@eleve.ensai.fr>
2025-09-05 09:18:15 +0000

    Modifications

---
+++ src/tests/test_business_object/test_pokemon/test_all_rounder_pokemon.py
@@ -0,0 +1,19 @@
+from business_object.pokemon.all_rounder_pokemon import AllRounderPokemon
+from business_object.statistic import Statistic
+
+
+class TestAllRounderPokemon:
+    def test_get_coef_damage_type(self):
+        # GIVEN
+        lucario = AllRounderPokemon(stat_current=Statistic(sp_atk=100, sp_def=100))
+
+        # WHEN
+        multiplier = lucario.get_pokemon_attack_coef()
+
+        # THEN
+        assert multiplier == 2
+
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__])
